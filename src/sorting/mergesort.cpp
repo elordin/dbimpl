39da630fbfbc4d6df42eb748baff21f0c6e77a52
Thomas Weber <weber@openinnovator.net>
2016-04-17 01:47:41 +0200

    Added merge

--- src/sorting/mergesort.cpp
+++ src/sorting/mergesort.cpp
@@ -9,32 +9,32 @@ using namespace std;
 
 void externalSort(int fdInput, uint64_t size, int fdOutput, uint64_t memSize) {
     // Number of partitions to fit one partition in memSize memory.
-    const uint k = 2; // size * sizeof(uint64_t) / memSize + 1;
-    
+    const uint k = size * sizeof(uint64_t) / memSize + 1;
+
     if (k < 2) {
         // All data fits into memory
         vector<uint64_t> partition(size);
 
         // Read from input file
         if (read(fdInput, &partition[0], size * sizeof(uint64_t)) < 1) {
-            cout << "No data found.\n";
+            cout << "No data found." << endl;
             return;
         }
-        
+
         // Sort
         sort(partition.begin(), partition.end());
- 
+
         // Write to output file
         if (posix_fallocate(fdOutput, 0, size * sizeof(uint64_t)) != 0) {
-            cout << "Could not allocate space for output file.\n";
+            cout << "Could not allocate space for output file." << endl;
         } else {
             if (write(fdOutput, &partition[0], size * sizeof(uint64_t)) < 0) {
-                cout << "Error writing to output file.\n";
+                cout << "Error writing to output file." << endl;
             }
         }
     } else {
         // uint partitionSize = memSize - (memSize % sizeof(uint64_t)); // Don't read partial numbers
-        
+
         // Byte size of the partition
         uint partitionSize = size / k * sizeof(uint64_t);
         // List of all tmp files
@@ -43,122 +43,131 @@ void externalSort(int fdInput, uint64_t size, int fdOutput, uint64_t memSize) {
         int tmpFd;
         int partitionNumber = -1;
         char filename[32];
-        
+
         // Read from input file
         while (read(fdInput, &buffer[0], partitionSize) > 0) {
-      
+
             // Sort
             sort(buffer.begin(), buffer.end());
-                        
+
             partitionNumber++;
-            
+
             // New filename
             sprintf(filename, ".tmpPart%i", partitionNumber);
-            
+
             if ((tmpFd = open(filename, O_CREAT | O_TRUNC | O_RDWR, S_IRUSR|S_IWUSR)) < 0) {
-                cout << "Could not open output file. Aborting.\n";
+                cout << "Could not open output file. Aborting." << endl;
                 return;
             }
             if (posix_fallocate(tmpFd, 0, partitionSize) != 0) {
-               cout << "Could not allocate file space for partition.\n";
+               cout << "Could not allocate file space for partition." << endl;
                 return;
-            }            
+            }
             if (write(tmpFd, &buffer[0], partitionSize) < 0) {
                 cout << "Error writing partition." << endl;
                 return;
             }
-            
-            tmpFds.push_back(tmpFd);
+            lseek(tmpFd, 0, SEEK_SET);
+            tmpFds[partitionNumber] = tmpFd;
         }
-        
-        cout << partitionNumber << endl;
-        
+
         if (partitionNumber < 0) {
             cout << "No data found." << endl;
-            return;    
+            return;
         }
-        
-        /*
-        uint entriesPerPartition = memSize / sizeof(uint64_t) / (partitionNumber + 1);
-        cout << entriesPerPartition << endl;
-        
-        vector<vector<uint64_t>> numbers(partitionNumber + 1);
-        
-        cout << numbers.size() << endl;
-        
-        for (int i = 0; i <= partitionNumber; i++) {
-            vector<uint64_t> partitionSegment(entriesPerPartition);
-            
-            if (read(tmpFds[i], &partitionSegment[0], entriesPerPartition * sizeof(uint64_t)) < 1) {
-                cout << "Empty partition found." << endl;
-            }
-            
-            reverse(partitionSegment.begin(), partitionSegment.end());
-            numbers[i] = partitionSegment;
-        }
-        
-        for (uint i = 0; i < numbers.size(); i++) {
-            for (uint j = 0; j < numbers[i].size(); j++) {
-                cout << numbers[i][j] << " | ";
-            }
-            cout << endl;
-        }
-        */
-        
-        /*
-        while (numbers.size() > 1) {
-            // Find smallest number
-            
-            int smallestIndex = 0; 
-            for (uint i = 0; i <= numbers.size(); i++) {
-                if (numbers[i].back() < numbers[smallestIndex].back()) {
-                    smallestIndex = i;
+
+        // Merge step
+        vector<vector<uint64_t>> segments(partitionNumber + 1);
+
+        // Number of numbers per segment
+        uint segmentSize = 5;
+
+        // Load the initial batch of numbers
+        for (int j = 0; j <= partitionNumber; j++) {
+            vector<uint64_t> segment(segmentSize);
+            if (read(tmpFds[j], &segment[0], segmentSize * sizeof(uint64_t)) > 0) {
+                // TODO resize if not enough data was read
+                reverse(segment.begin(), segment.end());
+                segments[j] = segment;
+            } else {
+                cout << "Error reading from partition " << j << endl;
+                for (int f = 0; f <= partitionNumber; f++) {
+                    close(tmpFds[f]);
                 }
-            }
-            if (write(fdOutput, &(numbers[smallestIndex].back()), sizeof(uint64_t)) < 0) {
-                cout << "Error writing result." << endl;
                 return;
             }
-            numbers[smallestIndex].pop_back();
-            if (numbers[smallestIndex].size() < 1) {
-                // Reload more data 
-                // If none is available, close and remove from numbers
+        }
+
+        // Make space for output
+        if (posix_fallocate(fdOutput, 0, size * sizeof(uint64_t)) != 0) {
+            cout << "Could not allocate space for output file." << endl;
+            for (int f = 0; f <= partitionNumber; f++) {
+                close(tmpFds[f]);
+            }
+            return;
+        }
+
+        // Merge by dumping smallest number into file and reloading data
+        while (segments.size() > 0) {
+            uint minIndex = 0;
+            uint64_t minElement = segments[0].back();
+
+            // Find smallest element
+            for (uint i = 0; i < segments.size(); i++) {
+                if (segments[i].back() < minElement) {
+                    minIndex = i;
+                    minElement = segments[i].back();
+                }
+            }
+            
+            // Write smallest element to output file
+            if (write(fdOutput, &minElement, sizeof(uint64_t)) < 0) {
+                cout << "Error writing to output file" << endl;
+                for (int f = 0; f <= partitionNumber; f++) {
+                    close(tmpFds[f]);
+                }
+                return;
+            }
+
+            // Remove smallest element from its segment
+            segments[minIndex].pop_back();
+            if (segments[minIndex].size() < 1) {
+                // Try loading new data
+                vector<uint64_t> segment(segmentSize);
+                if (read(tmpFds[minIndex], &segment[0], segmentSize * sizeof(uint64_t)) > 0) {
+                    // TODO resize if not enough data was read
+                    reverse(segment.begin(), segment.end());
+                    segments[minIndex] = segment;
+                } else {
+                    // Else remove segment from list of segments
+                    // TODO Remove tmp file
+                    close(tmpFds[minIndex]); 
+                    segments.erase(segments.begin() + minIndex);
+                }
             }
         }
-        */
-        // Merge tmp files into output file
-            // For each tmp file load first number
-            // Find smallest number and write it to ouput file
-            // Load next number from the file that contained the smallest number
-            // Repeat until all tmp files are exhausted
-
-        // Write last vector en bloc
-
-        // Remove tmp files
+        lseek(fdOutput, 0, SEEK_SET);
     }
 }
 
-int validateOutputFile(int fdOutput){
-	
+int validateOutputFile(int fdOutput) {
+
 	uint64_t oldElement = 0;
 	uint64_t nextElement;
 
-	while (read(fdOutput, &nextElement, sizeof(uint64_t)) > 0) {
-    		if (nextElement < oldElement) return EXIT_FAILURE;
-		//cout << nextElement << " größer als " << oldElement << endl;
-    		oldElement = nextElement;
-	}	
-
-	cout << "Validation successful" << endl;
-
-	return EXIT_SUCCESS;
+    cout << "Validating" << endl;
+    while (read(fdOutput, &nextElement, sizeof(uint64_t)) > 0) {
+        cout << nextElement << endl;
+        if (nextElement < oldElement) return -1;
+        oldElement = nextElement;
+    }
+    return 0;
 }
 
 
 int main(int argc, char **argv) {
     if (argc < 4) {
         cout << "Usage: ./main <input file> <output file> <memory in MB>" << endl;
-        return EXIT_SUCCESS;
     } else {
         // Get command line arguments
         char *inputFileName  = argv[1];
@@ -175,16 +184,22 @@ int main(int argc, char **argv) {
 
         if ((fdOutput = open(outputFileName, O_CREAT | O_TRUNC | O_WRONLY, S_IRUSR|S_IWUSR)) < 0) {
             cout << "Could not open output file. Aborting." << endl;
+            close(fdInput);
             return EXIT_FAILURE;
         }
         // Sort input to ouput
         // TODO Where do we get the size parameter?
         externalSort(fdInput, 20, fdOutput, memSize);
 
-        // Read output file
-        // Validate output file    
-	validateOutputFile(fdOutput);   
-
+        // Validate output file
+        lseek(fdOutput, 0, SEEK_SET);
+        if (validateOutputFile(fdOutput) != 0) {
+            cout << "Validation failed." << endl;
+            close(fdInput);
+            close(fdOutput);
+            return EXIT_FAILURE;
+        }
+        cout << "Validation successful" << endl;
         close(fdInput);
         close(fdOutput);
     }
