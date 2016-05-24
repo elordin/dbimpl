190b91839dcf429edb8efa877b359ac0c14b0748
Thomas Weber <weber@openinnovator.net>
2016-05-24 10:01:49 +0200

    value accessors

--- src/BTree/BTree.cpp
+++ src/BTree/BTree.cpp
@@ -1,11 +1,23 @@
+#include <cassert>
+
 #include "BTree.hpp"
 
+
+Key* BTree::nthKeyOfNode(char* node, unsigned n) {
+    assert(n < fanOut);
+    return reinterpret_cast<Key*>(node + sizeof(char*) + (sizeof(char*) + sizeof(Value)) * n);
+}
+
+char* BTree::nthPointerOfNode(char* node, unsigned n) {
+    assert(n <= fanOut);
+    return reinterpret_cast<Value*>(node + (sizeof(char*) + sizeof(Value)) * n);
+}
 
 char* BTree::findChildNode(char* node, Key key) {
 
 }
 
-Value BTree::getEntry(char* node, Key key) {
+Value* BTree::findEntry(char* node, Key key) {
 
 }
 
@@ -17,7 +29,7 @@ bool BTree::contains(char* node, Key key) {
 
 }
 
-BTree::BTree(unsigned fanOut) {
+BTree::BTree() {
 
 }
 
@@ -31,15 +43,15 @@ bool BTree::erase(Key key) {
 
 Value BTree::lookup(Key key) {
     std::function<Value(char*)> recLookup = [key, &recLookup](char* node) -> Value {
-        if (this->isLeaf(node) && this->contains(node, key)) {
+        if (BTree::isLeaf(node) && BTree::contains(node, key)) {
             // Key found
-            return this->getEntry(node, key);
-        } else if (this->isLeaf(node)) {
+            return BTree::getEntry(node, key);
+        } else if (BTree::isLeaf(node)) {
             // Key is not in the tree
             throw "Element not found.";
         } else {
             // Need to go deeper
-            return recLookup(this->findChildNode(node, key));
+            return recLookup(BTree::findChildNode(node, key));
         }
     };
     return recLookup(this->root);
--- src/BTree/BTree.hpp
+++ src/BTree/BTree.hpp
@@ -2,40 +2,59 @@
 
 #include "../SlottedPage/TID.hpp"
 
+#define FANOUT 2;
+
 template<
     class Key,
     class Value,
     class KeyEqual = std::equal_to<Key>
 >
 class BTree {
+
+    struct Node {
+        Node* children[FANOUT];
+        Key keys[FANOUT - 1];
+    }
+
+
  private:
-    unsigned fanOut;
     char* root;
+
+    /**
+     *  Returns the nth element in a node.
+     */
+    static Key* nthKeyOfNode(char* node, unsigned n);
+
+    /**
+     *  Returns nth pointer within a node, pointing to nth child.
+     */
+    static char* nthPointerOfNode(char* node, unsigned n);
 
     /**
      *  Returns child node that would presumably contain key.
      *  Throws an exception if node is a leaf node.
      */
-    char* findChildNode(char* node, Key key);
+    static char* findChildNode(char* node, Key key);
 
     /**
      *  Returns the entry with given key from given node.
      *  Throws an exception if node is not a leaf node.
      */
-    Value getEntry(char* node, Key key);
+    static Value findEntry(char* node, Key key);
 
     /**
      *  Returns whether a given node is a leaf node or not.
      */
-    bool isLeaf(char* node);
+    static bool isLeaf(char* node);
 
     /**
      *  Returns whether a given (leaf) node contains entry with given key.
      *  Throws an exception if node is not a leaf node.
      */
-    bool contains(char* node, Key key);
+    static bool contains(char* node, Key key);
+
  public:
-    BTree(unsigned fanOut);
+    BTree();
 
     /**
      *  Inserts the given value with associated key into the tree.
