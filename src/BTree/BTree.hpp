7408a41db09d3f3516fc0b60d06f077a0aa88ef7
Thomas Weber <weber@openinnovator.net>
2016-05-26 23:27:13 +0200

    On page lookup

--- src/BTree/BTree.hpp
+++ src/BTree/BTree.hpp
@@ -16,8 +16,10 @@ struct InnerNode {
 	uint64_t upper; //page-id of right-most child
 	unsigned num_keys;
 	Key keys[FANOUT - 1];
-	//char* children[fanOut];
+	uint64_t children[FANOUT];
+
     uint64_t getChildPageId(Key key);
+    bool contains(Key key);
 };
 
 template<class Key, class Value>
@@ -28,6 +30,9 @@ struct LeafNode {
 	unsigned num_keys;
 	Key keys[FANOUT - 1];
 	Value values[FANOUT - 1];
+
+    unsigned indexOfKey(Key key);
+    bool contains(Key key);
 };
 
 template<
@@ -46,62 +51,8 @@ class BTree {
     };
 
  private:
-    char* root;
     uint64_t rootPageId;
     BufferManager* bm;
-
-    /**
-     *  Returns the nth element in a node.
-     */
-    static key_type* nthKeyOfNode(char* node, unsigned n) {
-    }
-
-    /**
-     *  Returns nth pointer within a node, pointing to nth child.
-     */
-    static char* nthPointerOfNode(char* node, unsigned n) {
-    }
-
-    /**
-     *  Returns child node that would presumably contain key.
-     *  Throws an exception if node is a leaf node.
-     */
-    static char* findChildNode(char* node, key_type key) {
-    }
-
-    /**
-     *  Returns the entry with given key from given node.
-     *  Throws an exception if node is not a leaf node.
-     */
-    static value_type* findEntry(char* node, key_type key) {
-    }
-
-    /**
-     *  Returns whether a given node is a leaf node or not.
-     */
-    static bool isLeaf(char* node);
-
-    /**
-     *  Returns whether a given (leaf) node contains entry with given key.
-     *  Throws an exception if node is not a leaf node.
-     */
-    static bool contains(char* node, key_type key) {
-        if(!isLeaf(node)){
-            perror("Node is not a leaf node.");
-            throw "Node is not a leaf node.";
-        } else {
-            LeafNode<key_type, value_type>* leaf = reinterpret_cast<LeafNode<Key, Value>*>(node);
-            unsigned place = 0;
-            while((place < leaf->num_keys-1) && (leaf->keys[place]<key)) {
-                ++place;
-            }
-            if(leaf->keys[place+1] == key){
-                return true;
-            } else {
-                return false;
-            }
-        }
-    }
 
  public:
     BTree(BufferManager* bm) : bm(bm) {
@@ -120,7 +71,7 @@ class BTree {
                     perror("Key already in tree.");
                     throw "Key already in tree.";
                 } else {
-                    LeafNode<key_type, value_type>* leaf = 
+                    LeafNode<key_type, value_type>* leaf =
                         reinterpret_cast<LeafNode<key_type, value_type>*>(node);
                     // if there is enough space in the leaf (fanOut-2 at most)
                     if(leaf->num_keys <= FANOUT - 2){
@@ -139,7 +90,7 @@ class BTree {
                         return true;
                     } else {
                         // split leaf into two
-                        LeafNode<key_type, value_type>* new_leaf = 
+                        LeafNode<key_type, value_type>* new_leaf =
                             new LeafNode<key_type, value_type>();
                         new_leaf->num_keys = leaf->num_keys - (FANOUT/2);
                         for(unsigned j=0; j < new_leaf->num_keys; ++j) {
@@ -184,6 +135,7 @@ class BTree {
         return recFindNode(this->root);
     }
 
+    // TODO
     bool isLeaf(void* data) { return false; }
 
     /**
@@ -210,7 +162,9 @@ class BTree {
             }
         }
 
-        if (node->num_keys < 1 || this->contains(node, key)) {
+        if (node->num_keys > 0 && this->contains(node, key)) {
+            bm->unfixPage(*frame, false);
+            frame = &bm->fixPage(pageId, true);
             // Check for underflow
             // if (node->num_keys == FANOUT / 2) {
                 // Underflow
@@ -218,11 +172,11 @@ class BTree {
                 // TODO Currently ignoring underflow
                 unsigned index = this->findEntryByIndex(node, key);
                 node->num_keys--;
-                memcpy(node->values + index * sizeof(value_type), 
-                    node->values + (index + 1) * sizeof(value_type), 
+                memcpy(node->values + index * sizeof(value_type),
+                    node->values + (index + 1) * sizeof(value_type),
                     (FANOUT - 1 - index) * sizeof(value_type)
                 );
-                memcpy(node->keys + index * sizeof(key_type), 
+                memcpy(node->keys + index * sizeof(key_type),
                     node->keys + (index + 1) * sizeof(key_type),
                     (FANOUT - 1 - index) * sizeof(value_type)
                 );
@@ -236,31 +190,35 @@ class BTree {
     }
 
     /**
-     *  Returns the page from the tree presumably containing the key.
-     */
-    char* pageLookup(key_type key) {
-        std::function<LeafNode<key_type, value_type>*(char*)> recLookup = 
-            [this, key, &recLookup](char* node) -> LeafNode<key_type, value_type>* {
-            if (this->isLeaf(node)) {
-                return node;
-            } else {
-                return recLookup(findChildNode(node, key));
-            }
-        };
-        return recLookup(this->root);
-    }
-
-    /**
      *  Returns the value associated with key from the tree.
      *  Throws an exception if the value cannot be found.
      */
     Value* lookup(key_type key) {
-        LeafNode<key_type, value_type>* node = this->pageLookup(key);
-        if (this->contains(node, key)) {
-            return this->getEntry(node, key);
-        } else {
-            throw "Element not found.";        
+        // Find node
+        uint64_t pageId = this->rootPageId;
+        LeafNode<key_type, value_type>* node;
+        BufferFrame* frame = (BufferFrame*) malloc(sizeof(BufferFrame));
+        bool notFound = true;
+
+        while (notFound) {
+            frame = &(bm->fixPage(pageId, false));
+            auto data = frame->getData();
+            if ((notFound = isLeaf(data))) {
+                node = reinterpret_cast<LeafNode<key_type, value_type>*>(data);
+            } else {
+                InnerNode<key_type, value_type>* inner = reinterpret_cast<InnerNode<key_type, value_type>*>(data);
+                pageId = inner->getChildPageId(key);
+                bm->unfixPage(*frame, false);
+            }
         }
+        if (node->num_keys > 0 && node->contains(key)) {
+            Value* ret = (Value*) malloc(sizeof(Value));
+            *ret = node->values[node->indexOfKey(key)];
+            return ret;
+        } else {
+            throw "Not found.";
+        }
+
     }
 
 };
